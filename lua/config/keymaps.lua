0dd022b4885eb834ba2081e94aee39f734e61b6a
Mo Hank <hank@mbp.local>
2024-07-09 21:52:59 +0800

    lazyvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,37 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- dap keymapping to Jetbrains
+local dap = require("dap")
+vim.keymap.set("n", "<F8>", function()
+  dap.step_over()
+end)
+vim.keymap.set("n", "<F7>", function()
+  dap.step_into()
+end)
+vim.keymap.set("n", "<F9>", function()
+  dap.step_out()
+end)
+vim.keymap.set("n", "<F10>", function()
+  dap.continue()
+end)
+
+-- go to head/tail of a line
+vim.keymap.set({ "n", "i" }, "<M-S-left>", "^")
+vim.keymap.set({ "n", "i" }, "<M-S-right>", "$")
+
+-- Resize window fastly
+vim.keymap.set({ "n", "i" }, "<M-up>", ":res -5<cr>")
+vim.keymap.set({ "n", "i" }, "<M-down>", ":res +5<cr>")
+vim.keymap.set({ "n", "i" }, "<M-left>", ":vertical resize+5<cr>")
+vim.keymap.set({ "n", "i" }, "<M-right>", ":vertical resize-5<cr>")
+
+-- Go to window fastly
+vim.keymap.set({ "n", "i" }, "<leader><up>", "<C-w>k")
+vim.keymap.set({ "n", "i" }, "<leader><down>", "<C-w>j")
+vim.keymap.set({ "n", "i" }, "<leader><left>", "<C-w>h")
+vim.keymap.set({ "n", "i" }, "<leader><right>", "<C-w>l")
+
+-- zen mode
+vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>:ZenMode<CR>")
---
+++ lua/plugins/zen.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    event = "BufRead",
+    dependencies = { "lvimuser/lsp-inlayhints.nvim" },
+    config = function()
+      local lsp_inlayhints = require("lsp-inlayhints")
+      require("zen-mode").setup({
+        window = {
+          backdrop = 1,
+          height = 1.0,
+          width = 1.0,
+          options = {
+            signcolumn = "no",
+            number = true,
+            relativenumber = true,
+            cursorline = true,
+            cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+          },
+        },
+        plugins = {
+          gitsigns = { enabled = false },
+          tmux = { enabled = false },
+          twilight = { enabled = false },
+          -- this will change the font size on wezterm when in zen mode
+          -- See alse also the Plugins/Wezterm section in this projects README
+          wezterm = {
+            enabled = true,
+            -- can be either an absolute font size or the number of incremental steps
+            font = "+2", -- (10% increase per step)
+          },
+        },
+      })
+    end,
+  },
+}
