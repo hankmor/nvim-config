0dd0d7177a8cf78d3dce03f762c474a837cd5b0c
hank <hankmo@gmail.com>
2024-10-21 07:08:24 +0000

    update config

--- lua/lang/go.lua
+++ lua/lang/go.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("FileType", {
     -- set go specific options
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
-    vim.opt_local.colorcolumn = "120"
+    -- vim.opt_local.colorcolumn = "120"
   end,
 })
 
